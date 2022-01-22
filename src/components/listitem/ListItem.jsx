5a8f94577d098b67780b2472680ce7c91159fe09
Oreofreakshake <53213763+Oreofreakshake@users.noreply.github.com>
2022-01-22 20:17:05 +0500

    made a slider for animes

---
+++ src/components/listitem/ListItem.jsx
@@ -0,0 +1,12 @@
+import "./ListItem.scss";
+
+const ListItem = () => {
+    return <div className="listitem">
+        <img src="https://api-cdn.myanimelist.net/images/anime/1992/116576l.jpg" alt="cover"></img>
+
+
+
+    </div>;
+};
+
+export default ListItem;
